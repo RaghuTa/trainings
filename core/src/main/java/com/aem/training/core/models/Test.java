4fb59aa1554a51228aa6ffb7684ff1db6360d33a
RaghuTa <raghu.talluri835@gmail.com>
2022-11-20 18:19:25 +0530

    Added test class

---
+++ core/src/main/java/com/aem/training/core/models/Test.java
@@ -0,0 +1,15 @@
+package com.aem.training.core.models;
+
+public class Test {
+	void display()
+	{
+		System.out.println("This is to test Git repo of kiran");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t=new Test();
+		t.display();
+	}
+}
+
