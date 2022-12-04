ee4cf2d98854f031848f9e7dd8deed1e073b1724
RaghuTa <raghu.talluri835@gmail.com>
2022-12-04 13:33:46 +0530

    Int method added in Test

--- core/src/main/java/com/aem/training/core/models/Play.java
+++ core/src/main/java/com/aem/training/core/models/Play.java
@@ -5,11 +5,18 @@ public class Play {
 	{
 		System.out.println("This is to test Git repo of kiran");
 	}
+	int add(int a,int b)
+	{
+		int c=a+b;
+		System.out.println("the value of c:"+c);
+		return c;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Play t=new Play();
 		t.display();
+		t.add(45, 54);
 	}
 }
 
